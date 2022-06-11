c2f31510b445f6235bfaf3c647571ec8c2bdd14c
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-06-11 11:57:30 +0200

    react-app-rewired config for outside types

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { io } from 'socket.io-client';
+import { Chat } from './components/Chat';
 
-function App() {
+export function App() {
+  const socket = io('https://localhost:3001')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Encountered a type problem */}
+      {/* @ts-ignore:next-line */}
+      <Chat socket={socket}/>
     </div>
   );
 }
 
-export default App;
+
---
+++ src/components/Chat.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { Socket } from 'socket.io-client';
+import { MessageEntity } from 'types';
+
+export function Chat(socket: Socket) {
+  const [messages, setMessages] = useState<MessageEntity[]>([])
+  const [joined, setJoined] = useState<boolean>(false)
+  const [name, setName] = useState<boolean>(false)
+  const [messageText, setMessageText] = useState<string>('')
+
+  useEffect(() => {
+    socket.emit('findAllMessages', {}, (response: MessageEntity[]) => {
+        setMessages(response)
+    })
+
+    socket.on('message', (message: any) => {
+        setMessages([...messages, message])
+    })
+  }, [])
+
+  const join = () => {
+    socket.emit('join', {name: name}, (names: any) => {
+        setJoined(true)
+    })
+  }
+  const sendMessage = () => {
+    socket.emit('createMessage', { text: messageText }, (response: any) => {
+        setMessageText('')
+    })
+  }
+  return (
+    <div className="chat">
+        {messages.map((e) => {
+            return <p>{e}</p>
+        })}
+    </div>
+  );
+}
+
