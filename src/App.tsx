c081d00afc1d9678d03f863a27c7401494dc0043
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-06-14 18:17:02 +0200

    Added VideoPlayer+RoomList+UserList to app structure

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,16 @@ import { io } from 'socket.io-client';
 import { Chat } from './components/Chat';
 import { SocketProvider, SocketContext } from './context/Socket';
 import { NameForm } from './components/NameForm';
+import { RoomList } from './components/RoomList';
+import { UserList } from './components/UserList';
+import { VideoPlayer } from './components/VideoPlayer';
 
 export function App() {
   const socket = useContext(SocketContext)
   const [joined, setJoined] = useState<boolean>(false)
+  const [joinedRoom, setJoinedRoom] = useState<boolean>(false)
   const [name, setName] = useState<string>('')
+  const [room, setRoom] = useState<string>('')
   const join = (name: string) => {
     socket.emit('join', {name}, (names: any) => {
         setName(name)
@@ -19,8 +24,11 @@ export function App() {
   return (
     <SocketProvider>
     <div className="App">
-      {joined ? <Chat name={name}/> : <NameForm join={join}/>}
-      
+      {room ? <h2>You're in room #{room}</h2> : null}
+      {joinedRoom ? <VideoPlayer room={room} /> : null}
+      {joinedRoom ? <Chat name={name} room={room}/> : null}
+      {joinedRoom ? <UserList room={room}/> : null}
+      {joined ? <RoomList room={room} setJoinedRoom={setJoinedRoom} setRoom={setRoom} /> : <NameForm join={join}/>}
     </div>
     </SocketProvider>
   );
