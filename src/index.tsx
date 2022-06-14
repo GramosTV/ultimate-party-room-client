1d67d42cb7439a127753991ac3e4a7a4ab7095b2
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-06-14 18:17:34 +0200

    Had to disable react strict mode because of react-player bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './style/video.css';
 import './index.css';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +9,10 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  //Had to disable strict mode due to this bug: https://github.com/cookpete/react-player/issues/1453
+  <React.Fragment>
     <App />
-  </React.StrictMode>
+  </React.Fragment>
 );
 
 // If you want to start measuring performance in your app, pass a function
