01cd0bec3d4ce1a62d998bdf135f5ca76ce3c715
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-06-14 18:17:50 +0200

    Chat component improvements

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,33 +1,36 @@
-import React, { useContext, useMemo, useState } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import { SocketContext } from "../context/Socket";
 import { MessageEntity, IsTypingEntity } from "types";
 import { MessageInput } from "./MessageInput";
 interface ChatProps {
   name: string;
+  room: string;
 }
 export function Chat(props: ChatProps) {
+  const {name, room} = props;
   const [messages, setMessages] = useState<MessageEntity[]>([]);
   const socket = useContext(SocketContext);
   const [userIsTyping, setUserIsTyping] = useState<string>("");
-  useMemo(() => {
-    socket.emit("findAllMessages", {}, (response: MessageEntity[]) => {
+  useEffect(() => {
+    setMessages([]);
+    socket.emit("findAllMessages", {roomId: room}, (response: MessageEntity[]) => {
       setMessages(response);
     });
-
+  }, [room])
+  useMemo(() => {
     socket.on("message", (message: MessageEntity) => {
       setMessages(actualState => {
         return [...actualState, message]
     });
     });
-
     socket.on("typing", ({ name, isTyping }: IsTypingEntity) => {
       isTyping ? setUserIsTyping(`${name} is typing...`) : setUserIsTyping(``);
     });
   }, []);
   return (
     <div className="chat">
-      <span>Your name is {props.name}</span>
+      <span>Your name is {name}</span>
       {messages.map((e, i) => {
         return (
           <div key={i} className="message">
@@ -38,7 +41,7 @@ export function Chat(props: ChatProps) {
         );
       })}
       {userIsTyping ? <p>{userIsTyping}</p> : null}
-      <MessageInput name={props.name}/>
+      <MessageInput name={name} room={room}/>
     </div>
   );
 }
