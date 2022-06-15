a3c915c223d95750c925782ef8ba1931075bac6c
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-06-15 19:57:45 +0200

    Added profile pic upload + Sketch Canvas

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import { NameForm } from './components/NameForm';
 import { RoomList } from './components/RoomList';
 import { UserList } from './components/UserList';
 import { VideoPlayer } from './components/VideoPlayer';
+import { ProfilePicUpload } from './components/ProfilePicUpload';
+import { SketchCanvas } from './components/SketchCanvas';
 
 export function App() {
   const socket = useContext(SocketContext)
@@ -27,8 +29,9 @@ export function App() {
       {room ? <h2>You're in room #{room}</h2> : null}
       {joinedRoom ? <VideoPlayer room={room} /> : null}
       {joinedRoom ? <Chat name={name} room={room}/> : null}
+      {joinedRoom ? <SketchCanvas room={room} /> : null}
       {joinedRoom ? <UserList room={room}/> : null}
-      {joined ? <RoomList room={room} setJoinedRoom={setJoinedRoom} setRoom={setRoom} /> : <NameForm join={join}/>}
+      {joined ? <RoomList room={room} setJoinedRoom={setJoinedRoom} setRoom={setRoom} /> : <><NameForm join={join}/><ProfilePicUpload /></>}
     </div>
     </SocketProvider>
   );
