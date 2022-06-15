d86b588702acc3d86aee09c1d94b4c69830edb7a
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-06-15 19:57:21 +0200

    Import canvas style

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/video.css';
 import './index.css';
+import './style/canvas.css';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 
