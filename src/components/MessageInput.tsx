0b86ffc62d4073bac6dc9780d3a4c3ed5234118a
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-06-22 18:54:35 +0200

    restructure

--- src/components/MessageInput.tsx
+++ src/components/MessageInput.tsx
@@ -1,67 +1,61 @@
-import React, { useContext, useRef, useState } from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import { SocketContext } from "../context/Socket";
+import React, { useContext, useRef, useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { SocketContext } from '../context/Socket';
 import { MessageEntity } from 'types'
 interface MessageInputProps {
     name: string;
     room: string;
 }
-export const MessageInput = (props: MessageInputProps) => {
-  const {name, room} = props;
+export const MessageInput = ({name, room}: MessageInputProps) => {
   const socket = useContext(SocketContext);
-  const [messageText, setMessageText] = useState<string>('');
-  const sendMessage = () => {
-    socket.emit("createMessage", { name: props.name, text: messageText, roomId: props.room }, (response: MessageEntity) => {
-      setMessageText("");
-    });
+  const sendMessage = (text: string) => {
+    socket.emit('createMessage', { name: name, text, roomId: room });
   };
   let typingTimeout;
   const emitTyping = () => {
-    socket.emit("typing", { isTyping: true, roomId: room });
+    socket.emit('typing', { isTyping: true, roomId: room });
     typingTimeout = setTimeout(() => {
-      socket.emit("typing", { isTyping: false, roomId: room });
+      socket.emit('typing', { isTyping: false, roomId: room });
     }, 2000);
   };
   const handleChange = (e: React.ChangeEvent<any>) => {
-    setMessageText(e.currentTarget.value);
     emitTyping()
   }
   
   return (
-    <div>
+    <>
       <Formik
-        initialValues={{ message: "" }}
-        validate={(values) => {
-          const errors = { message: "" };
+        initialValues={{ message: '' }}
+        validate={values => {
+          const errors = { message: '' };
           if (!values.message) {
-            errors.message = "Required";
-          } else if (values.message.length < 1) {
-            errors.message = "Too short";
-          } else if (values.message.length > 100) {
-            errors.message = "Too long";
+            errors.message = 'Required';
+          } else if (values.message.length < 3) {
+            errors.message = 'Too short';
+          } else if (values.message.length > 255) {
+            errors.message = 'Too long';
           }
-          return {};
+          if (errors.message) {
+            return errors
+          }
+          return {}
         }}
-        onSubmit={(values, { setSubmitting }) => {
-            setMessageText(values.message);
-            sendMessage()
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+            sendMessage(values.message);
             setSubmitting(false);
-        //   setTimeout(() => {
-        //     alert(JSON.stringify(values, null, 2));
-        //     setSubmitting(false);
-        //   }, 400);
+            resetForm();
         }}
       >
         {({ isSubmitting }) => (
-          <Form>
-            <Field type="text" name="message" onChange={handleChange} value={messageText}/>
-            <ErrorMessage name="message" component="div" />
-            <button type="submit" disabled={isSubmitting}>
+          <Form className="chatForm">
+            <Field type='text' name='message' onKeyUp={handleChange} className='input'/>
+            <button className='confirmBtn' type='submit' disabled={isSubmitting}>
               Submit
             </button>
+            <ErrorMessage name='message' component='div' />
           </Form>
         )}
       </Formik>
-    </div>
+    </>
   );
 };
