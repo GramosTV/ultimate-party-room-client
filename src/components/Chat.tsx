66939fa8e4dd481186d53f0f9039c07646136344
Mikołaj Gramowski <emeraldbob020@gmail.com>
2022-06-22 18:54:22 +0200

    restructure

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,8 +1,7 @@
-import React, { useContext, useEffect, useMemo, useState } from "react";
-import { io, Socket } from "socket.io-client";
-import { SocketContext } from "../context/Socket";
-import { MessageEntity, IsTypingEntity } from "types";
-import { MessageInput } from "./MessageInput";
+import React, { useContext, useEffect, useMemo, useRef, useState } from 'react';
+import { SocketContext } from '../context/Socket';
+import { MessageEntity, IsTypingEntity } from 'types';
+import { MessageInput } from './MessageInput';
 interface ChatProps {
   name: string;
   room: string;
@@ -11,36 +10,51 @@ export function Chat(props: ChatProps) {
   const {name, room} = props;
   const [messages, setMessages] = useState<MessageEntity[]>([]);
   const socket = useContext(SocketContext);
-  const [userIsTyping, setUserIsTyping] = useState<string>("");
+  const [userIsTyping, setUserIsTyping] = useState<string>('');
   useEffect(() => {
     setMessages([]);
-    socket.emit("findAllMessages", {roomId: room}, (response: MessageEntity[]) => {
+    socket.emit('findAllMessages', {roomId: room}, (response: MessageEntity[]) => {
       setMessages(response);
     });
   }, [room])
   useMemo(() => {
-    socket.on("message", (message: MessageEntity) => {
+    socket.on('message', (message: MessageEntity) => {
+      console.log(message);
       setMessages(actualState => {
         return [...actualState, message]
     });
     });
-    socket.on("typing", ({ name, isTyping }: IsTypingEntity) => {
-      isTyping ? setUserIsTyping(`${name} is typing...`) : setUserIsTyping(``);
+    socket.on('typing', ({ name, isTyping }: IsTypingEntity) => {
+      isTyping ? setUserIsTyping(`${name} is typing...`) : setUserIsTyping('');
     });
   }, []);
+  const chat = useRef<null | HTMLDivElement>(null)
+  const scrollToBottom = () => {
+    if (chat.current?.offsetTop && chat.current.offsetHeight) {
+      chat.current.scrollTop = chat.current.scrollHeight;
+    }
+    
+  }
   return (
-    <div className="chat">
-      <span>Your name is {name}</span>
+    <div className='chatContainer'>
+    <h2 className='gradientText small'>Chat</h2>
+    <div className='chat' ref={chat}>
+      {/* <span>Your name is {name}</span> */}
       {messages.map((e, i) => {
+        setTimeout(() => {
+          scrollToBottom()
+        }, 100)
         return (
-          <div key={i} className="message">
+          <div key={i} className='message'>
+            <span>{e.name}:<p>{new Date(String(e.createdAt))?.toISOString().replace('T',' ').substring(11, 19)}</p></span>
             <p>
-              {e.name}: {e.text}
+              {e.text}
             </p>
           </div>
         );
       })}
-      {userIsTyping ? <p>{userIsTyping}</p> : null}
+    </div>
+    {userIsTyping ? <p>{userIsTyping}</p> : null}
       <MessageInput name={name} room={room}/>
     </div>
   );
